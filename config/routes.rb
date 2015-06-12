7345ff662a4a16aa3afdc952d175ca6f06dca3fa
Nathan Noble <nathan.noble@gmail.com>
2015-06-11 23:15:17 -0400

    added logon status

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :comments
   end
     
-  root 'welcome#index'
+  root to: 'welcome#index'
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
