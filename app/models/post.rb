ec40431e0382a39cbcff84c58328a56167bfe0a3
Nathan Noble <nathan.noble@gmail.com>
2015-06-26 20:30:19 -0400

    added search in maine page

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,9 +9,45 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   
   accepts_nested_attributes_for :images, :comments
-  validates :title, presence: true,
-                      length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5 }
+  
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'snowball', index_options: 'offsets'
+      indexes :text, analyzer: 'snowball'
+    end
+  end             
                       
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'text']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            text: {}
+          }
+        }
+      }
+    )
+  end
+                  
 end
 
-Post.import # for auto sync model with elastic search
+# Delete previous index
+Post.__elasticsearch__.client.indices.delete index: Post.index_name rescue nil
+ 
+# Create the new index
+Post.__elasticsearch__.client.indices.create \
+  index: Post.index_name,
+  body: { settings: Post.settings.to_hash, mappings: Post.mappings.to_hash }
+ 
+# Index all posts
+Post.import
